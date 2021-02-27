a641b18731cd49469206f288c70ae8b73a477d92
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-02-26 20:23:40 -0600

    roughed out post route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -12,6 +12,15 @@ module.exports = function (app) {
   });
 
   //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+  app.post("/api/notes", function (req, res) {
+    //receive new note to save on request body. ==> add a unique id to each new note.
+
+    //add to the db.json file
+
+    //return new note to client
+    // res.send("this will add stuff to things");
+    console.log("does it still work?");
+  });
 
   //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 };
